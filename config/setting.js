c24047236e86c52e72e4f3edbbca434b1bcba7fc
Tela634 <veronicawaithira680@gmail.com>
2025-04-08 15:00:36 +0300

    Update setting.js

--- config/setting.js
+++ config/setting.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const chalk = require("chalk");
-global.creAtor = "yournumber@s.whatsapp.net";
-global.owner = ["yournumber"];
-global.ownerNumber = ["yournumber@s.whatsapp.net"];
-global.nomerOwner = "yournumber";
-global.namabotnya = "AbotMD";
-global.namaownernya = "Muq";
+global.creAtor = "254706519089@s.whatsapp.net";
+global.owner = ["254706519089"];
+global.ownerNumber = ["254706519089@s.whatsapp.net"];
+global.nomerOwner = "254706519089";
+global.namabotnya = "LESTA_ELIUD";
+global.namaownernya = "ELIUD";
 
 //Web Api
 global.APIs = {
